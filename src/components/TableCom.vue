a8d2f94aa073c04a51a952f0c21a2abdfa50ecf5
张三 <13119625212@163.com>
2024-03-10 06:01:28 +0800

    完善tableCom

--- src/components/TableCom.vue
+++ src/components/TableCom.vue
@@ -1,7 +1,30 @@
 <template>
   <el-table :data="tableData" border :row-key="rowKey" size="large">
     <slot></slot>
-    <el-table-column v-for="(item, index) in columns" :key="index" :prop="item.prop" :label="item.label" row-key="id" align="center" />
+    <el-table-column
+      v-for="(item, index) in columns"
+      :key="index"
+      :prop="item.prop"
+      :label="item.label"
+      row-key="id"
+      show-overflow-tooltip
+      align="center"
+      @selection-change="$emit('selectionChange', $event)"
+    />
+    <el-table-column label="操作" align="center">
+      <template #default="scope">
+        <el-button size="small" text @click="$emit('handleEdit', scope.$index, scope.row)"
+          >编辑</el-button
+        >
+        <el-button
+          size="small"
+          type="danger"
+          text
+          @click="$emit('handleDel', scope.$index, scope.row)"
+          >删除</el-button
+        >
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
--- src/views/auth/UserView.vue
+++ src/views/auth/UserView.vue
@@ -1,30 +1,46 @@
 <template>
   <div>
-    <table-com :columns="columns" :tableData="tableData">
+    <table-com
+      :columns="columns"
+      :tableData="tableData"
+      @handle-edit="handleEdit"
+      @handle-del="handleDel"
+      @selection-change="selectionChange"
+    >
       <el-table-column type="selection"></el-table-column>
     </table-com>
   </div>
 </template>
 
 <script setup>
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive } from 'vue'
 import { getUser } from '@/api/user.js'
-onMounted(()=>{
-  getUser().then(res => {
+onMounted(() => {
+  getUser().then((res) => {
     console.log(res)
     tableData.push(...res.data)
   })
 })
-  const columns = [
-    {prop:'id',label:'id'},
-    {prop:'name',label:'用户名'},
-    {prop:'email',label:'邮箱'},
-    {prop:'role',label:'角色'},
-    {prop:'date',label:'修改时间'},
-  ]
-  const tableData = reactive([])
+const columns = [
+  { prop: 'id', label: 'id' },
+  { prop: 'name', label: '用户名' },
+  { prop: 'email', label: '邮箱' },
+  { prop: 'role', label: '角色' },
+  { prop: 'date', label: '修改时间' }
+]
+const tableData = reactive([])
+
+function handleEdit(index, row) {
+  console.log('编辑')
+  console.log(index, row)
+}
+function handleDel(index, row) {
+  console.log('删除')
+  console.log(index, row)
+}
+function selectionChange(val) {
+  console.log(val, '多选')
+}
 </script>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
